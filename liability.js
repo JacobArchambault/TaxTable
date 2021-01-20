8ad629ed90859a5e10068ac0a9274ed02f0d8b63
Jacob Archambault <jacob.archambault@waystar.com>
2021-01-20 11:47:32 -0500

    move liability into its own file and fix typo in isBetween method

---
+++ liability.js
@@ -0,0 +1,61 @@
+function debug(){
+    let income = document.getElementById("income").value;
+    if (lowestBracket(income)){
+        display(lowestLiability(income));
+    }
+
+}
+function displayLiability() {
+    let income = document.getElementById("income").value;
+    if (lowestBracket(income)){
+        display(lowestLiability(income));
+    }
+    if (bracketTwo(income)){
+        display(bracketTwoLiability(income));
+    }
+    if (bracketThree(income)){
+        display(bracketThreeLiability(income));
+    }
+    if (bracketFour(income)){
+        display(bracketFourLiability(income));
+    }
+    if (bracketFive(income)){
+        display(bracketFiveLiability(income));
+    }            
+}
+function isBetween(number, lowValue, highValue) {
+    return number >= lowValue && number < highValue;
+}
+function lowestBracket(number) {
+    return isBetween(number, 0, 27050);
+}
+function bracketTwo(number) {
+    return isBetween(number, 27050, 65550);
+}
+function bracketThree(number) {
+    return isBetween(number, 65550, 136750);
+}
+function bracketFour(number) {
+    return isBetween(number, 136750, 297350);
+}
+function bracketFive(number) {
+    return number >= 297350;
+}
+function lowestLiability(gross) {
+    return gross * 0.15;
+}
+function bracketTwoLiability(gross) {
+    return 4057.5 + (gross - 27050) * 0.275;
+}
+function bracketThreeLiability(gross) {
+    return 14654 + (gross - 65550) * 0.305;
+}
+function bracketFourLiability(gross) {
+    return 36361 + (gross - 136750) * 0.355;
+}
+function bracketFiveLiability(gross) {
+    return 93374 + (gross - 297350) * 0.391;
+}
+function display(number){
+    document.getElementById("taxable-amount").value = number;        
+}
