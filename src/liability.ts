21fb077423ec61fa7fe65b1f22346a5b50402f3a
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-08 13:16:42 -0500

    add inferred types with VS Code for all ts parameters

--- src/liability.ts
+++ src/liability.ts
@@ -16,43 +16,43 @@ function displayLiability() {
         display(asCurrency(bracketFiveLiability(income)));
     }            
 }
-function isBetween(number, lowValue, highValue) {
+function isBetween(number: number, lowValue: number, highValue: number) {
     return number >= lowValue && number < highValue;
 }
-function lowestBracket(number) {
+function lowestBracket(number: any) {
     return isBetween(number, 0, 27050);
 }
-function bracketTwo(number) {
+function bracketTwo(number: any) {
     return isBetween(number, 27050, 65550);
 }
-function bracketThree(number) {
+function bracketThree(number: any) {
     return isBetween(number, 65550, 136750);
 }
-function bracketFour(number) {
+function bracketFour(number: any) {
     return isBetween(number, 136750, 297350);
 }
-function bracketFive(number) {
+function bracketFive(number: number) {
     return number >= 297350;
 }
-function lowestLiability(gross) {
+function lowestLiability(gross: number) {
     return gross * 0.15;
 }
-function bracketTwoLiability(gross) {
+function bracketTwoLiability(gross: number) {
     return 4057.5 + (gross - 27050) * 0.275;
 }
-function bracketThreeLiability(gross) {
+function bracketThreeLiability(gross: number) {
     return 14654 + (gross - 65550) * 0.305;
 }
-function bracketFourLiability(gross) {
+function bracketFourLiability(gross: number) {
     return 36361 + (gross - 136750) * 0.355;
 }
-function bracketFiveLiability(gross) {
+function bracketFiveLiability(gross: number) {
     return 93374 + (gross - 297350) * 0.391;
 }
-function display(number){
+function display(number: string){
     document.getElementById("taxable-amount")["value"] = number;        
 }
-function asCurrency(number){
+function asCurrency(number: number | bigint){
     return new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD'}).format(number);
